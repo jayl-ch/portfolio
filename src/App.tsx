2a90c4285c18477e936b070b980430e75a24725d
jayl-ch <jaychicogo328@gmail.com>
2026-02-07 17:35:06 +0800

    feat: added see all tech stack with react router

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import "./styles/App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import { TechStackFull } from "./components/Main/TechStack";
 import useDark from "./hooks/dark-theme";
 import { setDark } from "./lib/body.toggle";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   const { isDark, toggleIsDark } = useDark();
@@ -12,11 +14,19 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-col gap-4">
-        <Header isDark={isDark} toggleIsDark={toggleIsDark} />
-        <Main />
-        <Footer />
-      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="flex flex-col gap-4">
+              <Header isDark={isDark} toggleIsDark={toggleIsDark} />
+              <Main />
+            </div>
+          }
+        />
+        <Route path="/techstack" element={<TechStackFull />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
--- src/components/Main/TechStack.tsx
+++ src/components/Main/TechStack.tsx
@@ -2,19 +2,25 @@ import { Card } from "../ui/card";
 import { techstack } from "@/mocks/data";
 import TechStackIcon from "../../icons/techstack";
 import type { TechStackItem } from "@/types/types";
+import { Link } from "react-router-dom";
 
 const TechStack = () => {
   return (
-    <Card className="gap-2">
-      <div className="flex items-center gap-2">
-        <TechStackIcon />
-        <h2 className="font-bold text-xl">Tech Stack</h2>
+    <Card className="gap-2 overflow-hidden">
+      <div className="flex justify-between">
+        <div className="flex items-center gap-2">
+          <TechStackIcon />
+          <h2 className="font-bold text-xl">Tech Stack</h2>
+        </div>
+        <Link to="/techstack">
+          See all <span className="text-gray-400">&gt;</span>
+        </Link>
       </div>
-      {techstack.map(({ category, tools }: TechStackItem) => (
+      {techstack.slice(0, 3).map(({ category, tools }: TechStackItem) => (
         <div key={category} className="flex flex-col gap-2">
           <h3 className="font-bold">{category}</h3>
           <div className="flex gap-1 p-0 flex-wrap">
-            {tools.map((tool) => (
+            {tools.slice(0, 5).map((tool) => (
               <p
                 key={tool}
                 className="text-xs inline border rounded-sm px-2 py-1"
@@ -26,6 +32,35 @@ const TechStack = () => {
         </div>
       ))}
     </Card>
+  );
+};
+
+// view all stacks
+export const TechStackFull = () => {
+  return (
+    <div className="max-w-250 mx-auto flex flex-col gap-5 animate-appear">
+      <div className="flex gap-2 items-center">
+        <Link to="/">
+          <span className="text-gray-400">&lt;</span> Back to home
+        </Link>
+        <h2 className="font-bold text-2xl">Tech Stack</h2>
+      </div>
+      {techstack.map(({ category, tools }: TechStackItem) => (
+        <div key={category} className="flex flex-col gap-2">
+          <h3 className="font-bold text-lg">{category}</h3>
+          <div className="flex gap-1 p-0 flex-wrap">
+            {tools.map((tool) => (
+              <p
+                key={tool}
+                className="text-sm inline border-2 rounded-sm px-2 py-1"
+              >
+                {tool}
+              </p>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
   );
 };
 
