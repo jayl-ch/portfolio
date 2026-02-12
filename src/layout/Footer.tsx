fc1d2def637ee3463f4ab20f399d02a7c9a2e263
jayl-ch <jaychicogo328@gmail.com>
2026-02-12 23:06:44 +0800

    style: adjust the max-width of entire layout

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -3,7 +3,7 @@ import { profile } from "../mocks/data.json";
 const Footer = () => {
   return (
     <footer className="mt-5 animate-appear">
-      <div className="max-w-250 mx-auto text-center border-t py-5">
+      <div className="max-w-220 mx-auto text-center border-t py-5">
         &copy; 2026 {profile.name}. All Rights Reserved.
       </div>
     </footer>
